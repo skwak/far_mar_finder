3b8fe5edb2d995758cf9d00b157d39159210ebaf
Stephanie Kwak <stephanieyoungkwak@gmail.com>
2014-09-15 15:20:30 -0700

    Add self.all and initialize market.

--- lib/far_mar/market.rb
+++ lib/far_mar/market.rb
@@ -1,4 +1,23 @@
 module FarMar
   class Market
+
+    def initialize(row)
+      @id = row[0]
+      @name = row[1]
+      @address = row[2]
+      @city = row[3]
+      @county = row[4]
+      @state = row[5]
+      @zip = row[6]
+    end
+
+    def self.all
+      csv = CSV.read "support/markets.csv"
+      csv.collect { |row| Market.new(row) }
+    end
+
+    def self.find(id)
+      
+    end
   end
 end
